3a8e8f71b2db10e1b96c1db6e714f895248c37bc
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-07 23:41:18 +0100

    omarchy blev lige flækket af en opdatering så blev lige nød til at genoprette

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from player import Player
+
+from constants import *
+from logger import log_state
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
