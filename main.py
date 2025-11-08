ca62f88ae7353c68b40fe9e3bae8f98227c63443
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-08 15:15:24 +0100

    nu kan det skyde

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from asteroidfield import AsteroidField
 from constants import *
 from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -18,10 +19,12 @@ def main():
     drawable = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
 
+    AsteroidField.containers = updatable
     asteroidfield = AsteroidField()
 
     Player.containers = (drawable, updatable)
